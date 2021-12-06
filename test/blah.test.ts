f321c5a880df2cb63613683ca5f91de67cc40ca3
jamaaldev <kabarojamaal@gmail.com>
2021-12-06 04:06:12 +0000

    added test function for all features

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,7 +1,19 @@
-import { DaysToCome } from '../src';
+import { DaysToCome, JointDate, DaysGone } from '../src';
 
-describe('blah', () => {
+describe('DaysToCome', () => {
   it('works', () => {
     expect(new DaysToCome('').YearsOld('2000-12-02')).toEqual([21]);
+  });
+});
+describe('JointDate', () => {
+  it('works', () => {
+    expect(new JointDate().JointDay('2000-12-02', '')).toEqual('December 2000');
+  });
+});
+describe('DaysGone', () => {
+  it('works', () => {
+    expect(
+      new DaysGone('').DaysGoneBirthDay('2000-12-02', '15', 'p', 'nice')
+    ).toEqual('<p class="nice">  4   </p>');
   });
 });
